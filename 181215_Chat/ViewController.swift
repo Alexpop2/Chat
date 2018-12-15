f8758f2069543e5625ae4a1e20d173be71ea577d
Рабочий <rabocij@MacBook-Pro-Aleksej-2.local>
2018-12-15 16:56:37 +0300

    Initial Commit

---
+++ 181215_Chat/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  181215_Chat
+//
+//  Created by Рабочий on 15/12/2018.
+//  Copyright © 2018 Рабочий. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
